07564431a16a5f894d94c8d0fa9ed7cf394aee17
haeusian <haeusian@students.zhaw.ch>
2023-03-22 11:15:24 +0100

    Change Hardware pin config
    Change variables to fit in new RF_Module

--- thermomed-device/src/rt/Rf_hardware.cpp
+++ thermomed-device/src/rt/Rf_hardware.cpp
@@ -2,26 +2,22 @@
 
 #include <Arduino.h>
 
-#define ADC_VAC1_Pin                GPIO_PIN_2
-#define ADC_VAC1_GPIO_Port          GPIOC
 
-#define ADC_VAC2_Pin                GPIO_PIN_3
-#define ADC_VAC2_GPIO_Port          GPIOC
 
-#define RF_ADC_FEEDBACK_Pin         GPIO_PIN_3
+#define RF_ADC_FEEDBACK_CURRENT     GPIO_PIN_3
 #define RF_ADC_FEEDBACK_GPIO_Port   GPIOA
 
-#define RF_DAC_CONTROL_Pin          GPIO_PIN_4
-#define RF_DAC_CONTROL_GPIO_Port    GPIOA
+#define RF_ADC_FEEDBACK_PHASE       GPIO_PIN_4
+#define RF_ADC_FEEDBACK_GPIO_Port   GPIOA
+
+#define RF_ADC_FEEDBACK_VDC         GPIO_PIN_6
+#define RF_ADC_FEEDBACK_GPIO_Port   GPIOA
 
 #define RF_ENABLE_Pin               GPIO_PIN_4
 #define RF_ENABLE_GPIO_Port         GPIOC
 
-#define RF_OPAMP_ENABLE_Pin         GPIO_PIN_5
-#define RF_OPAMP_ENABLE_GPIO_Port   GPIOC
-
 #define RF_DEBUG_Pin                GPIO_PIN_2
-#define RF_DEBUG_GPIO_Port         GPIOA
+#define RF_DEBUG_GPIO_Port          GPIOA
 
 #define ADC_12BIT                   4096.0f
 #define ADC_VCC                     3.3f
@@ -30,8 +26,6 @@ const float Rf_hardware::GAIN_CONTROL_MIN = 0.0f;
 const float Rf_hardware::GAIN_CONTROL_MAX = 3.0f;
 
 static TIM_HandleTypeDef            htim2;
-static ADC_HandleTypeDef            hadc1;
-static DAC_HandleTypeDef            hdac1;
 
 Rf_hardware::Rf_hardware()
 {
@@ -76,120 +70,7 @@ Rf_hardware::beginTimer2()
     }
 }
 
-void
-Rf_hardware::beginADC1()
-{
 
-    ADC_MultiModeTypeDef    multimode           = {0};
-    ADC_ChannelConfTypeDef  sConfig             = {0};
-    GPIO_InitTypeDef        GPIO_InitStruct     = {0};
-
-    /** Common config */
-    hadc1.Instance                              = ADC1;
-    hadc1.Init.ClockPrescaler                   = ADC_CLOCK_SYNC_PCLK_DIV4;
-    hadc1.Init.Resolution                       = ADC_RESOLUTION_12B;
-    hadc1.Init.DataAlign                        = ADC_DATAALIGN_RIGHT;
-    hadc1.Init.GainCompensation                 = 0;
-    hadc1.Init.ScanConvMode                     = ADC_SCAN_DISABLE;
-    hadc1.Init.EOCSelection                     = ADC_EOC_SINGLE_CONV;
-    hadc1.Init.LowPowerAutoWait                 = DISABLE;
-    hadc1.Init.ContinuousConvMode               = DISABLE;
-    hadc1.Init.NbrOfConversion                  = 1;
-    hadc1.Init.DiscontinuousConvMode            = DISABLE;
-    hadc1.Init.ExternalTrigConv                 = ADC_EXTERNALTRIG_T2_TRGO;
-    hadc1.Init.ExternalTrigConvEdge             = ADC_EXTERNALTRIGCONVEDGE_RISING;
-    hadc1.Init.DMAContinuousRequests            = DISABLE;
-    hadc1.Init.Overrun                          = ADC_OVR_DATA_PRESERVED;
-    hadc1.Init.OversamplingMode                 = DISABLE;
-
-    /*** MspInit BEGIN ***/
-
-    /* Peripheral clock enable */
-    __HAL_RCC_ADC12_CLK_ENABLE();
-
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    /** ADC1 GPIO Configuration  */
-    GPIO_InitStruct.Pin                         = ADC_VAC1_Pin|ADC_VAC2_Pin;
-    GPIO_InitStruct.Mode                        = GPIO_MODE_ANALOG;
-    GPIO_InitStruct.Pull                        = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin                         = RF_ADC_FEEDBACK_Pin;
-    GPIO_InitStruct.Mode                        = GPIO_MODE_ANALOG;
-    GPIO_InitStruct.Pull                        = GPIO_NOPULL;
-    HAL_GPIO_Init(RF_ADC_FEEDBACK_GPIO_Port, &GPIO_InitStruct);
-
-    /* ADC1 interrupt Init */
-    HAL_NVIC_SetPriority(ADC1_2_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(ADC1_2_IRQn);
-
-    /*** MspInit END ***/
-    
-    /* uses HAL_ADC_MspInit() internally, but unused! */
-    if (HAL_ADC_Init(&hadc1) != HAL_OK) {
-        Error_Handler();
-    }
-
-    /** Configure the ADC multi-mode */
-    multimode.Mode                              = ADC_MODE_INDEPENDENT;
-    if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK) {
-        Error_Handler();
-    }
-
-    /** Configure Regular Channel */
-    sConfig.Channel                             = ADC_CHANNEL_4;
-    sConfig.Rank                                = ADC_REGULAR_RANK_1;
-    sConfig.SamplingTime                        = ADC_SAMPLETIME_2CYCLES_5;
-    sConfig.SingleDiff                          = ADC_SINGLE_ENDED;
-    sConfig.OffsetNumber                        = ADC_OFFSET_NONE;
-    sConfig.Offset                              = 0;
-    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
-        Error_Handler();
-    }
-}
-
-void
-Rf_hardware::beginDAC1()
-{
-    DAC_ChannelConfTypeDef  sConfig             = {0};
-    GPIO_InitTypeDef        GPIO_InitStruct     = {0};
-
-    /*** MspInit BEGIN ***/
-
-    /* Peripheral clock enable */
-    __HAL_RCC_DAC1_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    /** DAC1 GPIO Configuration */
-    GPIO_InitStruct.Pin                         = RF_DAC_CONTROL_Pin;
-    GPIO_InitStruct.Mode                        = GPIO_MODE_ANALOG;
-    GPIO_InitStruct.Pull                        = GPIO_NOPULL;
-    HAL_GPIO_Init(RF_DAC_CONTROL_GPIO_Port, &GPIO_InitStruct);
-
-    /*** MspInit END ***/
-
-    /** DAC Initialization */
-    hdac1.Instance = DAC1;
-    if (HAL_DAC_Init(&hdac1) != HAL_OK) {
-        Error_Handler();
-    }
-
-    /** DAC channel OUT1 config */
-    sConfig.DAC_HighFrequency                   = DAC_HIGH_FREQUENCY_INTERFACE_MODE_AUTOMATIC;
-    sConfig.DAC_DMADoubleDataMode               = DISABLE;
-    sConfig.DAC_SignedFormat                    = DISABLE;
-    sConfig.DAC_SampleAndHold                   = DAC_SAMPLEANDHOLD_DISABLE;
-    sConfig.DAC_Trigger                         = DAC_TRIGGER_NONE;
-    sConfig.DAC_Trigger2                        = DAC_TRIGGER_NONE;
-    sConfig.DAC_OutputBuffer                    = DAC_OUTPUTBUFFER_ENABLE;
-    sConfig.DAC_ConnectOnChipPeripheral         = DAC_CHIPCONNECT_EXTERNAL;
-    sConfig.DAC_UserTrimming                    = DAC_TRIMMING_FACTORY;
-    if (HAL_DAC_ConfigChannel(&hdac1, &sConfig, DAC_CHANNEL_1) != HAL_OK) {
-        Error_Handler();
-    }
-}
 
 void
 Rf_hardware::beginGpio()
@@ -200,12 +81,10 @@ Rf_hardware::beginGpio()
     __HAL_RCC_GPIOC_CLK_ENABLE();
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(GPIOC, RF_OPAMP_ENABLE_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(GPIOC, RF_ENABLE_Pin, GPIO_PIN_SET);
     HAL_GPIO_WritePin(RF_ENABLE_GPIO_Port, RF_DEBUG_Pin, GPIO_PIN_RESET);
 
     /*Configure GPIO pins : RF_ENABLE_Pin */
-    GPIO_InitStruct.Pin = RF_ENABLE_Pin | RF_OPAMP_ENABLE_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -224,28 +103,14 @@ void
 Rf_hardware::begin()
 {
     beginTimer2();
-    beginADC1();
-    beginDAC1();
     beginGpio();
 
-    /* Calibrate w/o start */
-    if (HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED) != HAL_OK) {
-        Error_Handler();
-    }
     
     /* Start TIM2 and ADC1 */
 	if (HAL_TIM_Base_Start(&htim2) != HAL_OK) {
         Error_Handler();
     }
 
-	if (HAL_ADC_Start_IT(&hadc1) != HAL_OK) {
-        Error_Handler();
-    }
-
-    /* Start DAC1 */
-    if (HAL_DAC_Start(&hdac1, DAC_CHANNEL_1) != HAL_OK) {
-        Error_Handler();
-    }
 }
 
 
@@ -262,17 +127,8 @@ Rf_hardware::pke_disable()
     HAL_GPIO_WritePin(RF_ENABLE_GPIO_Port, RF_ENABLE_Pin, GPIO_PIN_SET);
 }
 
-void
-Rf_hardware::set_opamp_on_input_float()
-{
-    HAL_GPIO_WritePin(RF_OPAMP_ENABLE_GPIO_Port, RF_OPAMP_ENABLE_Pin, GPIO_PIN_RESET);
-}
 
-void
-Rf_hardware::set_opamp_on_input_pull_low()
-{
-    HAL_GPIO_WritePin(RF_OPAMP_ENABLE_GPIO_Port, RF_OPAMP_ENABLE_Pin, GPIO_PIN_SET);
-}
+
 
 void
 Rf_hardware::set_debug_pin_state(bool state)
@@ -292,7 +148,7 @@ Rf_hardware::set_debug_pin_state(bool state)
 float
 Rf_hardware::read_adc_voltage()
 {
-    return ADC_VCC * static_cast<float>(HAL_ADC_GetValue(&hadc1)) / ADC_12BIT;
+    //// READ I FEEDBACK HERE IAN
 }
 
 /**
@@ -308,14 +164,10 @@ Rf_hardware::set_dac_voltage(float v)
     } else if (v > GAIN_CONTROL_MAX) {
         v = GAIN_CONTROL_MAX;
     }
-
+        //// Set VOLTAGE HERE IAN
     uint32_t value = static_cast<uint32_t>(ADC_12BIT * v / ADC_VCC);
-    HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, value);
+
 }
 
-/*** IRQ Handler ************************************************************/
-extern "C" void
-ADC1_2_IRQHandler(void)
-{
-    HAL_ADC_IRQHandler(&hadc1);
-}
+/// WRITE NEW FUNCTION TO READ PHASE HERE IAN
+/// WRITE NEW FUNCTION TO READ VDC HERE IAN
